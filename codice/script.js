44385d6efdaa5233305bdfca76ca2f8d3603962c
Aploide Project <96500694+Aploide@users.noreply.github.com>
2022-03-02 12:02:33 +0100

    fixed audio starting too late

--- codice/script.js
+++ codice/script.js
@@ -1,8 +1,8 @@
 var password=document.getElementById("password");
 function love() {
     var audio = new Audio('./codice/song.mp3');
+    audio.play();
     var gif = document.getElementById("video")
     gif.setAttribute("style", "opacity: 100%;")
-    audio.play();
     audio.loop=true;
 }
